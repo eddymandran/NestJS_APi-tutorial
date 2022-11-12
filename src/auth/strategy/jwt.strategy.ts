02ee058281c470a1bd13c8c0a79a01b7c582715c
eddymandran <eddy.mandran@gmail.com>
2022-11-12 21:49:47 +0100

    NestJs guards

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,15 +1,38 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import {
+  ExtractJwt,
+  Strategy,
+} from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+  'jwt',
+) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  async validate(payload: {
+    sub: number;
+    email: string;
+  }) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: { id: payload.sub },
+      });
+    delete user.hash;
+    return user;
   }
 }
