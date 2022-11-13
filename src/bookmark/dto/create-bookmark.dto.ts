e60845000ab9d7b70740e6fc94a8698eec8e4f0b
eddymandran <eddy.mandran@gmail.com>
2022-11-13 16:12:13 +0100

    Bookmarks e2e test

---
+++ src/bookmark/dto/create-bookmark.dto.ts
@@ -0,0 +1,19 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateBookmarkDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  link: string;
+}
